7bb5693574b853e44bb1019f30dcd10d10f67d7f
MrASyntex <continentaldispatch@gmail.com>
2022-05-17 16:17:15 -0700

    Solar System 2 has stardet

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// write your code here
+        SolarSystem Xfac = new SolarSystem();
+        Xfac.Moon = "1";
+        Xfac.Star = "60";
+        Xfac.Planet ="12";
+        Xfac.Sun = "1";
+
+    }
+}
